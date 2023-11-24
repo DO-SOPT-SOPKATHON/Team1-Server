f6ca85339d3032bb5ab6ef312f05080531a59ec7
이혜연 <tjsalsu@naver.com>
2023-11-25 00:34:46 +0900

    init: Swagger 설정

---
+++ src/main/java/sopkathon/team1/global/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package sopkathon.team1.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "🚀[솝커톤 API 명세서]",
+                description = "안드1팀 API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+}
+
