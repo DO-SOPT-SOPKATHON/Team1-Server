087406099743b685c4192d0e4c818edac74a840c
parkheeddong <phj7214go@gmail.com>
2023-11-25 23:18:35 +0900

    feat: Category Many To One 관계 추가

--- src/main/java/sopkathon/team1/domain/post/Post.java
+++ src/main/java/sopkathon/team1/domain/post/Post.java
@@ -2,19 +2,25 @@ package sopkathon.team1.domain.post;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sopkathon.team1.domain.category.Category;
+import sopkathon.team1.domain.BaseEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post {
+public class Post extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
@@ -22,13 +28,17 @@ public class Post {
     private String title;
     private String content;
 
-    @Column(name = "created_date")
-    private LocalDate createdDate;
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 
     @Builder
-    public Post(String title, String content, LocalDate createdDate) {
+    public Post(String title, String content, LocalDate createdAt) {
         this.title = title;
         this.content = content;
-        this.createdDate = createdDate;
+        this.createdAt = createdAt;
     }
 }
