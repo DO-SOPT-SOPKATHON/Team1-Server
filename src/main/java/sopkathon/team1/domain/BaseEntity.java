4d8b0c42d1b4b630d72d1eb7d17511dac42a9502
parkheeddong <phj7214go@gmail.com>
2023-11-26 03:06:40 +0900

    refactor: base entity getter  추가

--- src/main/java/sopkathon/team1/domain/BaseEntity.java
+++ src/main/java/sopkathon/team1/domain/BaseEntity.java
@@ -4,12 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
